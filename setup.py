269fbd835e76b6bb3f9a765101c6b75dad0f2690
sousamd <sousamd_jose@gmail.com>
2020-02-24 18:03:59 +0000

    unittests update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'optimModels',
-    version = '0.0.10',
+    version = '0.0.16',
     package_dir = {'': 'src'},
     packages = find_packages('src'),
     include_package_data = True,
@@ -13,6 +13,7 @@ setup(
     license = 'Apache License Version 2.0',
     keywords = 'strain design',
     url = 'https://github.com/saragcorreia/optimModels.git',
-    long_description = open('README.rst').read(),
+    long_description = open('README.md').read(),
+    long_description_content_type = "text/markdown",
     classifiers = ['Topic :: Utilities', 'Programming Language :: Python :: 3.6', ],
     )
